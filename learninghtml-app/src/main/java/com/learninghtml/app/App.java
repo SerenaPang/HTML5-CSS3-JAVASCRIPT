9643c4132e73680518aba02b4ffe2f19438d0d66
SerenaPang4 <ypang4@usfca.edu>
2025-02-26 12:29:24 -0800

    added table.html, updated pom xml and controller

--- learninghtml-app/src/main/java/com/learninghtml/app/App.java
+++ learninghtml-app/src/main/java/com/learninghtml/app/App.java
@@ -1,13 +1,25 @@
 package com.learninghtml.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * Hello world!
- *
+ * This class does save user, update user, delete user, find all users in the
+ * website.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World !" );
-    }
+@Controller
+public class App {
+
+	@RequestMapping("/home")
+	public String home(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "home.html";
+	}
+
+
 }
